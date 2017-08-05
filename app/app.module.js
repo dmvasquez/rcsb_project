d9e55eb4a00e57953e90c0d186ee937d2c8b5bdf
David Vasquez <david.m.vasquez14@gmail.com>
2017-08-04 23:19:19 -0700

    updated js for reactive side nav

--- app/app.module.js
+++ app/app.module.js
@@ -1,7 +1,5 @@
 (function() {
     'use strict';
 
-    angular.module('module', [
-        
-    ]);
+    angular.module('app', []);
 })();
---
+++ app/search.controller.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('SearchController', SearchController);
+
+    SearchController.$inject = ['$http', '$timeout'];
+    function SearchController($http, $timeout) {
+        var vm = this;
+        var original = "";
+        vm.resultSet = [];
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            $.getJSON("HIV_LeftSideRefinements.json", function(leftSideRef) {
+            original = JSON.stringify(leftSideRef, null, 4);
+            document.getElementById("daCode").innerHTML = original;
+            $timeout(function() {
+                vm.resultSet = $.parseJSON(original);
+                console.log(vm.resultSet);            
+            }, 50);
+        })
+         }
+        
+    }
+})();
