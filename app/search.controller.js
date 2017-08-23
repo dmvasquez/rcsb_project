ecbd5d3cf826c9e472658d0ee2680a46a0e2f5fb
David Vasquez <david.m.vasquez14@gmail.com>
2017-08-22 22:26:45 -0700

    updated first 25 search result set JSON into project. Used Angular to ng-repeat set based on objects. Filtering coming soon after.

--- app/search.controller.js
+++ app/search.controller.js
@@ -9,7 +9,9 @@
     function SearchController($http, $timeout) {
         var vm = this;
         var original = "";
+        var firstTwentyFive = "";
         vm.resultSet = [];
+        vm.firstResults = [];
 
         activate();
 
@@ -17,14 +19,22 @@
 
         function activate() {
             $.getJSON("HIV_LeftSideRefinements.json", function(leftSideRef) {
-            original = JSON.stringify(leftSideRef, null, 4);
-            //document.getElementById("daCode").innerHTML = original;
-            $timeout(function() {
-                vm.resultSet = $.parseJSON(original);
-                console.log(vm.resultSet);            
-            }, 50);
-        })
-         }
+                original = JSON.stringify(leftSideRef, null, 4);
+                //document.getElementById("daCode").innerHTML = original;
+                $timeout(function() {
+                    vm.resultSet = $.parseJSON(original);
+                    //console.log(vm.resultSet);            
+                }, 50);
+            })
+            $.getJSON("HIV_SearchResults_ClassicRCSB_First25.json", function(searchResults) {
+                firstTwentyFive = JSON.stringify(searchResults, null, 4);
+                //document.getElementById("daCode").innerHTML = firstTwentyFive;
+                $timeout(function() {
+                    vm.firstResults = $.parseJSON(firstTwentyFive);
+                    console.log(vm.firstResults);            
+                }, 50);
+            })
+        }
         
     }
 })();
